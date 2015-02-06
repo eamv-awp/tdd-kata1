fd5670e4d4c925eccf969fcf90748ed5ff9e6aba
Anders Petersen <awp@eamv.dk>
2015-02-06 11:46:30 +0100

    Refactored handling of delimiter

--- tdd-kata1/src/StringCalculator.java
+++ tdd-kata1/src/StringCalculator.java
@@ -1,12 +1,15 @@
 public class StringCalculator {
-	
 	public int add(String numbers) {
+		String delimiter = ",";
+		
 		if (numbers == null || numbers.length() == 0)
 			return 0;
-		else if (!numbers.contains(","))
+		else if (!numbers.contains(delimiter))
 			return Integer.parseInt(numbers);
-		else
-			return Integer.parseInt(numbers.substring(0, numbers.indexOf(","))) + Integer.parseInt(numbers.substring(numbers.indexOf(",") + 1));
+		else {
+			int indexOfDelimiter = numbers.indexOf(delimiter);
+			return Integer.parseInt(numbers.substring(0, indexOfDelimiter)) + Integer.parseInt(numbers.substring(indexOfDelimiter + 1));
+		}
 	}
 
 }
